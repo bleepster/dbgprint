c7622ec8cdd683ec772bbfed0eab2f4e821fd042
Earl R. Lapus <earl.lapus@gmail.com>
2016-12-09 20:13:10 +0800

    small updates: - separate header declarations - add comments to header files - update how clean is done - consider osx scenario where directories are   created

--- dbgprint.h
+++ dbgprint.h
@@ -3,15 +3,22 @@
 
 #if defined (DBGPRINT_SYSLOG)
   #include <syslog.h>
+#elif defined (DBGPRINT_CONSOLE)
+  #include <stdio.h>
+#else
+  /* include nothing */
+#endif
+
+#if defined (DBGPRINT_SYSLOG)
   #define DP_DEBUG LOG_DEBUG
   #define DP_ERROR LOG_ERR
   #define DBGPRINT(priority, text, args...) syslog(priority, text, ##args)
 #elif defined (DBGPRINT_CONSOLE)
-  #include <stdio.h>
   #define DP_DEBUG stdout
   #define DP_ERROR stderr
   #define DBGPRINT(file, text, args...) fprintf(file, text, ##args)
 #else
+  /* define with blanks */
   #define DP_DEBUG
   #define DP_ERROR
   #define DBGPRINT(x,y,z...)
